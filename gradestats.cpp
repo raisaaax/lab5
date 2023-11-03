89e5e4fbd5ac37da671d4bf1f073dbfd7ff430f4
Raisa Silea <rs1753@live.mdx.ac.uk>
2023-11-03 23:05:44 +0000

    initial commit

---
+++ gradestats.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  const int MAX_GRADES = 100;
+  int grades[MAX_GRADES];
+  int numGrades = 0;
+  int total = 0;
+
+  cout << "Enter grade (-1 to end)" <<endl;
+
+  for (int i = 0; i<MAX_GRADES; i++){
+    int grade;
+    cin >> grade;
+    if (grade == -1){
+      break;
+    }
+    if (grade >=0 && grade <=MAX_GRADES){
+      grades[numGrades] = grade;
+      numGrades++;
+    }else{
+      cout << "invalid" <<endl;
+    }
+  }
+
+}
