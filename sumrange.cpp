00b525443d4edccc4d19ff5e84fd0d3104039950
Raisa Silea <rs1753@live.mdx.ac.uk>
2023-11-03 20:37:19 +0000

    final commit

--- sumrange.cpp
+++ sumrange.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+  int n, sum, num1, num2;
+
+  cout << "Enter two numbers";
+  cin >> num1 >> num2;
+  sum = 0;
+  for (n = num1; n <=num2; n++){
+    sum += n;
+  }
+  cout << "The sum from "<< num1<<" and "<<num2<<" is "<< sum<<endl;
+}
+
+
