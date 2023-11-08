85cc9531f8e25e77c48f28582257688b772a85f4
Raisa Silea <rs1753@live.mdx.ac.uk>
2023-11-08 16:31:32 +0000

    initial commit - not checking primes properly

---
+++ primes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPrime(int n){
+  if (n<= 1){
+    return false;
+  }
+  for (int i = 2; i*i <+n; ++i){
+    if (n % i==0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+int main(){
+  int limitNumber;
+
+  cout<< "Enter an integer: ";
+  cin >> limitNumber;
+
+  if (limitNumber < 2) {
+  cout << "There are no primes <= "<< limitNumber<<endl;
+  return 0;
+  }
+
+  cout << "Primes to "<<limitNumber << " are ";
+  for ( int i = 2; i <= limitNumber; ++i){
+    if (isPrime(i)){
+      cout <<i << " ";      
+    }
+ }
+ return 0;
+}
